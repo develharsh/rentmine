f510bb97475502966f84a98642a06e998f7e33c0
cpbhai <harshvsingh.223@gmail.com>
2021-08-16 20:42:44 +0530

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,53 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import cookie from 'js-cookie'
+
+const NavBar = () => {
+
+    const router = useRouter()
+    let user = false, uper = false
+    const cookCheck = cookie.get('uuuuu')
+    if (cookCheck) {
+        user = true;
+        if (cookCheck === '--10879') {
+            uper = true;
+        }
+    }
+
+    const isActive = (r) => {
+        if (r === router.pathname) {
+            return "active"
+        } else {
+            return ""
+        }
+    }
+
+    return (
+        <div>
+            <nav style={{ width: "100%" }}>
+                <div className="nav-wrapper #e040fb purple accent-2">
+                    <Link href="/"><a className="brand-logo left">Rent Mine</a></Link>
+                    <ul id="nav-mobile" className="right">
+                        {user
+                            ? <>
+                                <li className={isActive('/create')}><Link href="/create"><a>Create</a></Link></li>
+                                <li><button className="btnLogOut"
+                                    onClick={(e) => { cookie.remove('token'); cookie.remove('lvl'); cookie.remove('wh'); router.push('/login'); M.toast({ html: 'Logged Out', classes: 'green' }) }}>
+                                    Log Out
+                                </button></li>
+                            </>
+                            :
+                            <>
+                                <li className={isActive('/login')}><Link href="/login"><a>Login</a></Link></li>
+                                <li className={isActive('/signup')}><Link href="/signup"><a>Signup</a></Link></li>
+                            </>
+                        }
+                    </ul>
+                </div>
+            </nav>
+            <div style={{padding:"3px"}}></div>
+        </div>
+    )
+}
+
+export default NavBar
--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  reactStrictMode: true,
+  env: {
+    "BASE_URL": "http://localhost:3000",
+    "MONGODB_URL": "mongodb+srv://hvs:thakur@cluster0.h3zc7.mongodb.net/rentmine?retryWrites=true&w=majority",
+    "JWT_SECRET": "!)@(#*$&%^",
+    "TOKEN_EXPIRE":"7d",
+    "CLOUD_UPDATE_PRESET": "duskanddawn",
+    "CLOUD_NAME": "duskanddawn",
+    "CLOUD_API": "https://api.cloudinary.com/v1_1/duskanddawn/image/upload"
+  },
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
 }
